91e7794bd59d61890c7426a51a8d35f459b3715c
Ivan-G-Popov <i_g_p_@outlook.com>
2020-10-27 16:22:23 +0200

    Submit a solution

---
+++ Basic Syntax, Conditional Statements and Loops - Lab/13. Holidays Between Two Dates/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Data;
+using System.Globalization;
+
+namespace _13._Holidays_Between_Two_Dates
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Broken Code for Refactoring
+            /*
+             var startDate = DateTime.ParseExact(Console.ReadLine(),
+            "dd.m.yyyy", CultureInfo.InvariantCulture);
+        var endDate = DateTime.ParseExact(Console.ReadLine(),
+            "dd.m.yyyy", CultureInfo.InvariantCulture);
+        var holidaysCount = 0;
+        for (var date = startDate; date <= endDate; date.AddDays(1))
+            if (date.DayOfWeek == DayOfWeek.Saturday &&
+                date.DayOfWeek == DayOfWeek.Sunday) holidaysCount++;
+        Console.WriteLine(holidaysCount); 
+             */
+            DateTime startDate = DateTime.ParseExact(Console.ReadLine(), "d.M.yyyy", CultureInfo.InvariantCulture);
+            DateTime endDate = DateTime.ParseExact(Console.ReadLine(), "d.M.yyyy", CultureInfo.InvariantCulture);
+            int holidaysCount = 0;
+
+            for (DateTime date = startDate; date <= endDate; date = date.AddDays(1))
+            {
+                if (date.DayOfWeek == DayOfWeek.Saturday || date.DayOfWeek == DayOfWeek.Sunday)
+                {
+                    holidaysCount++;
+                }
+            }
+            Console.WriteLine(holidaysCount);
+        }
+    }
+}
